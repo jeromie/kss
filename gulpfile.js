9b9de9f3434fe0243f4c5eeb3cb77c8490c9e1a0
jeromie <jeromie@ajency.in>
2018-08-14 16:47:37 +0530

    gulp changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,47 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+'use strict';
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src('./themes/kss/static/scss/custom.scss')
-        .pipe(sass())
-        .pipe(gulp.dest("./themes/kss/static/css"))
-        .pipe(browserSync.stream());
+var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+var del = require('del');
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
+// Set the browser that you want to support
+const AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+// Gulp task to minify CSS files
+gulp.task('styles', function () {
+  return gulp.src('./themes/kss/static/scss/custom.scss')
+    // Compile SASS files
+    .pipe(sass({
+      outputStyle: 'nested',
+      precision: 10,
+      includePaths: ['.'],
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    // Auto-prefix css styles for cross browser compatibility
+    // .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    // Minify the file
+    .pipe(csso())
+    // Output
+    .pipe(gulp.dest('./themes/kss/static/css'))
 });
 
-
 // Static Server + watching scss/html files
-gulp.task('watch', ['sass'], function() {
+gulp.task('watch', ['styles'], function() {
     gulp.watch(['./themes/kss/static/scss/*.scss'], ['sass']);
 });
 
