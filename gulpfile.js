f021f792df16d5a8218bedc2edbad08091c89065
Amit <amit@ajency.in>
2018-09-04 18:45:11 +0530

    minify of css and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,56 @@
-var gulp        = require('gulp');
-var csso = require('gulp-csso');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
+var gulp = require('gulp'),
+    cleanCSS = require('gulp-clean-css'),
+    concat = require('gulp-concat')
+    uglify = require('gulp-uglify')
+    csso = require('gulp-csso');
+    prefix = require('gulp-autoprefixer')
+    sass = require('gulp-sass');
+
+// Minifies JS
+gulp.task('scripts', function(){
+    return gulp.src(['./themes/kss/static/js/bootstrap.min.js', './themes/kss/static/js/jquery.fancybox.js', './themes/kss/static/js/slick.min.js', './themes/kss/static/js/jquery.mmenu.all.js', './themes/kss/static/js/jquery.mousewheel.min.js', './themes/kss/static/js/lightgallery-all.min.js', './themes/kss/static/js/lazysizes.min.js', './themes/kss/static/js/slick.min.js'])
+    .pipe(uglify())
+    .pipe(concat('combine.js'))
+    .pipe(gulp.dest('./themes/kss/static/js'))
+});
+
+/*==========  Minify and concat different styles files  ==========*/
+
+// SASS Version
+gulp.task('sass', function(){
     return gulp.src('./themes/kss/static/scss/custom.scss')
-        .pipe(sass())
-        // Minify the file
-        .pipe(csso())
-        .pipe(gulp.dest("./themes/kss/static/css"))
-        .pipe(browserSync.stream());
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./themes/kss/static/css'))
+});
+
+// SCSS Version
+//gulp.task('styles', function(){
+    //return gulp.src('src/scss/**/*.scss')
+    //.pipe(sass())
+    //.pipe(prefix('last 2 versions'))
+    //.pipe(concat('main.css'))
+    //.pipe(minifyCSS())
+    //.pipe(gulp.dest('public/css'))
+//});
+
+
+// CSS Version
+
+gulp.task('css', function(){
+    return gulp.src('./themes/kss/static/css/*.css')
+     .pipe(csso())
+     .pipe(concat('combine.css'))
+   .pipe(gulp.dest('./themes/kss/static/css'))
+});
+
+gulp.task('default', function() {
+    gulp.run('scripts')
+    gulp.run('css')
+    gulp.watch('./themes/kss/static/scss/*.scss', function(){
+        gulp.run('sass')
+    })
 });
 
 
-// Static Server + watching scss/html files
-gulp.task('watch', ['sass'], function() {
-    gulp.watch(['./themes/kss/static/scss/*.scss'], ['sass']);
-});
-
-gulp.task('default', ['watch']);
-// gulp.task('default');
