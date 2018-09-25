b97e4ad326c485f48043330cf367925302bbc7bb
Amit <amit@ajency.in>
2018-09-25 10:39:27 +0530

    compressed images

--- themes/kss/static/js/custom.js
+++ themes/kss/static/js/custom.js
@@ -464,9 +464,9 @@ $(function() {
 $(document).ready(function(){
     $('.cd-add-to-cart').on('click',function(){
         //Scroll to top if cart icon is hidden on top
-        $('html, body').animate({
-            'scrollTop' : $(".shopping-cart").position().top
-        });
+        $(".cart-counter").removeClass('d-none'), 100;
+        $(".cart-counter").addClass('d-block'), 100;
+         $('#output').html(function(i, val) { return val*1+1 });
         //Select item image and pass to the function
         var itemImg = $(this).closest('.container').find('img').eq(1);
         flyToElement($(itemImg), $('.shopping-cart'));
